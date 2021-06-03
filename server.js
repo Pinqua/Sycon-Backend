bc7e09107ad4725f9be26ddb1f441851c64ab94b
Pinqua <piyushsati1999@gmail.com>
2021-06-03 18:50:57 +0530

    mongodb url error fixed

--- server.js
+++ server.js
@@ -14,12 +14,15 @@ const io = require("socket.io")(server, {
 const PORT = process.env.PORT || 9000;
 const defaultValue = "";
 
-mongoose.connect(`${process.env.MONGO_CONNECTION_URL}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  "mongodb+srv://PiyushSati:jXsOEExrxPeVeoSJ@cluster0.m0aq8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
 const connection = mongoose.connection;
 connection
   .once("open", () => {
